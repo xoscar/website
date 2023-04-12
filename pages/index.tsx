e89615530ab11704895ad5582dc5d70543da44d0
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-04-11 22:12:27 -0600

    updating home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Oscar R. Reyes</title>
         <meta name="description" content="Oscar R. Reyes Website" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/avatar.ico" />
       </Head>
 
       <div className={styles.circleContainer}>
@@ -19,8 +19,8 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <div className={styles.imageContainer}>
           <Image
-            layout="fixed"
-            src="/avatar.jpg"
+            layout="fill"
+            src="/avatar.png"
             alt="oscar-reyes"
             width={450}
             height={316}
@@ -32,6 +32,17 @@ const Home: NextPage = () => {
             Lead Software Engineer • Gamer • Creator
           </h2>
         </div>
+
+        <iframe
+          style={{ borderRadius: "12px" }}
+          src="https://open.spotify.com/embed/playlist/5LqsAqopEZrQH1REIxVOfU?utm_source=generator&theme=0"
+          width="100%"
+          height="152"
+          frameBorder="0"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        />
       </main>
 
       <footer className={styles.footer}>
