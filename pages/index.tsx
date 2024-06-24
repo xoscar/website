f02ab6efeb8330f13973b9da510dd9935287b12b
Oscar Reyes <oscar-rreyes1@hotmail.com>
2024-06-24 17:02:04 -0500

    chore: updating styles and photo

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,25 +20,19 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <div className={styles.imageContainer}>
-          <Image layout="fill" src="/avatar.png" alt="oscar-reyes" />
+          <Image
+            src="/avatar.jpeg"
+            alt="oscar-reyes"
+            width="0"
+            height="0"
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+          />
         </div>
         <h1 className={styles.title}>Oscar R. Reyes</h1>
         <div>
-          <h2 className={styles.description}>
-            Lead Software Engineer • Gamer • Creator
-          </h2>
+          <h2 className={styles.description}>Lead Software Engineer</h2>
         </div>
-
-        <iframe
-          style={{ borderRadius: "12px" }}
-          src="https://open.spotify.com/embed/playlist/5LqsAqopEZrQH1REIxVOfU?utm_source=generator&theme=0"
-          width="100%"
-          height="152"
-          frameBorder="0"
-          allowFullScreen
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-        />
       </main>
 
       <footer className={styles.footer}>
@@ -48,22 +42,6 @@ const Home: NextPage = () => {
           rel="noreferrer"
         >
           LinkedIn
-        </a>{" "}
-        |
-        <a
-          href="https://twitter.com/oreyes_io"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Twitter
-        </a>{" "}
-        |
-        <a
-          href="https://www.youtube.com/channel/UCBwVUFWn9Q3qL1XyHtvH-lg"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Youtube
         </a>{" "}
         |
         <a
