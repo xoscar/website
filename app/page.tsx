c4b5b91d3fd96a4d7f771d24ade6991a93911e5d
Oscar Reyes <oscar-rreyes1@hotmail.com>
2025-10-15 15:09:47 -0600

    updating links

--- app/page.tsx
+++ app/page.tsx
@@ -7,32 +7,30 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <main className={`${styles.main}`}>
         <h1 className={`${styles.title} ${styles.name}`}>Oscar R. Reyes</h1>
+        <h2 className={styles.title}>Fullstack Lead Software Engineer</h2>
+        <h3 className={styles.title}>AWS | React | Node.js | Docker | OpenTelemetry</h3>
       </main>
 
       <footer className={styles.footer}>
+        <a href="https://www.linkedin.com/in/oscar-rreyes/" target="_blank" rel="noreferrer">
+          LinkedIn
+        </a>{' '}
         <a
-          href="https://drive.google.com/file/d/16IZ50zgampXTaWe4GYexgFIaNi8UEUiP/view"
+          href="https://drive.google.com/file/d/13b8XG1ceMNggzaMo-_jhSOrmwVVHBN00/view?usp=sharing"
           target="_blank"
           rel="noreferrer"
         >
           Resume
         </a>{' '}
-        |
-        <Link href="/experiments" rel="noreferrer">
-          Experiments
-        </Link>{' '}
-        |
         <a href="https://github.com/xoscar" target="_blank" rel="noreferrer">
           Github
         </a>{' '}
-        |
-        <a href="https://www.linkedin.com/in/oscar-rreyes/" target="_blank" rel="noreferrer">
-          LinkedIn
-        </a>{' '}
-        |
         <a href="https://medium.com/@oscar-reyes" target="_blank" rel="noreferrer">
           Medium
         </a>
+        <Link href="/experiments" rel="noreferrer">
+          Experiments
+        </Link>{' '}
       </footer>
     </div>
   );
